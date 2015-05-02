b25be2e653bcbc7e328d2154aab0ccbefad2c3f2
José Ornelas <up200900381@fc.up.pt>
2015-05-02 11:53:40 +0000

    - Removidos botões settings desnecessários.
    git-svn-id: https://svn.dcc.fc.up.pt/projects/pbrandao/MyDiabetes@179 8fe4487d-d89a-4624-bb73-233bb408955e

--- src/com/jadg/mydiabetes/DataTools.java
+++ src/com/jadg/mydiabetes/DataTools.java
@@ -33,7 +33,7 @@ public class DataTools extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.data_tools, menu);
+		//getMenuInflater().inflate(R.menu.data_tools, menu);
 		return true;
 	}
 
--- src/com/jadg/mydiabetes/ImportExport.java
+++ src/com/jadg/mydiabetes/ImportExport.java
@@ -91,7 +91,7 @@ public class ImportExport extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.import_export, menu);
+		//getMenuInflater().inflate(R.menu.import_export, menu);
 		return true;
 	}
 
