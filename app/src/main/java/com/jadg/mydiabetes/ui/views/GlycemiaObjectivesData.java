ca8b53e4b8e3a7ec5682080aea1bcb7855995fac
Eduardo Soares <eduardosoares.pt@gmail.com>
2015-11-23 15:42:11 +0000

    Adds UI to add glycemia objectives in the setup process
    Similar to the add insulins.

---
+++ app/src/main/java/com/jadg/mydiabetes/ui/views/GlycemiaObjectivesData.java
@@ -0,0 +1,127 @@
+package com.jadg.mydiabetes.ui.views;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.text.TextUtils;
+
+import java.io.Serializable;
+
+public class GlycemiaObjectivesData implements Serializable, Parcelable {
+	public static final int NO_ERROR = 0;
+	public static final int ERROR_REPEATED_DESCRIPTION = 1;
+	public static final int ERROR_EMPTY_DESCRIPTION = 2;
+	public static final int ERROR_EMPTY_OBJECTIVE = 3;
+	public static final int ERROR_EMPTY_START_TIME = 4;
+	public static final int ERROR_EMPTY_END_TIME = 5;
+	public static final Creator<GlycemiaObjectivesData> CREATOR = new Creator<GlycemiaObjectivesData>() {
+		@Override
+		public GlycemiaObjectivesData createFromParcel(Parcel in) {
+			return new GlycemiaObjectivesData(in);
+		}
+
+		@Override
+		public GlycemiaObjectivesData[] newArray(int size) {
+			return new GlycemiaObjectivesData[size];
+		}
+	};
+	String description;
+	String startTime;
+	String endTime;
+	private int objective = -1;
+	int visibilityState;
+	int error = NO_ERROR;
+	int pox;
+
+
+	public GlycemiaObjectivesData(int pox) {
+		this.pox = pox;
+	}
+
+	protected GlycemiaObjectivesData(Parcel in) {
+		description = in.readString();
+		startTime = in.readString();
+		endTime = in.readString();
+		objective = in.readInt();
+		visibilityState = in.readInt();
+		error = in.readInt();
+		pox = in.readInt();
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public int getObjective() {
+		return objective;
+	}
+
+	public String getObjectiveAsString() {
+		if (objective == -1) {
+			return null;
+		} else {
+			return String.valueOf(objective);
+		}
+	}
+
+	public void setObjective(int objective) {
+		this.objective = objective;
+	}
+
+	public void setObjective(String objective) {
+		if (TextUtils.isEmpty(objective)) {
+			this.objective = -1;
+		} else {
+			this.objective = Integer.parseInt(objective, 10);
+		}
+	}
+
+
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel parcel, int i) {
+		parcel.writeString(description);
+		parcel.writeInt(objective);
+	}
+
+	public int getPosition() {
+		return pox;
+	}
+
+	public void setPosition(int position) {
+		this.pox = position;
+	}
+
+	public boolean isValid() {
+		error = NO_ERROR;
+		if (TextUtils.isEmpty(description)) {
+			error = ERROR_EMPTY_DESCRIPTION;
+		}
+		if (TextUtils.isEmpty(startTime)) {
+			error = ERROR_EMPTY_START_TIME;
+		}
+		if (TextUtils.isEmpty(endTime)) {
+			error = ERROR_EMPTY_END_TIME;
+		}
+		if (objective == -1) {
+			error = ERROR_EMPTY_OBJECTIVE;
+		}
+		return error == NO_ERROR;
+	}
+
+	public void setInvalid() {
+		visibilityState = GlycemiaObjetivesElement.MODE_EDIT;
+		if (isValid()) {
+			// then the problem is a repeated name
+			error = ERROR_REPEATED_DESCRIPTION;
+		}
+	}
+}
