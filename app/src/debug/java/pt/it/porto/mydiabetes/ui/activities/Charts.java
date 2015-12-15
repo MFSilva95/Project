43111e97a128afee801305b04369e66f29385c78
Eduardo Soares <eduardosoares.pt@gmail.com>
2015-12-15 21:14:04 +0000

    Added more tests and chart samples

--- app/src/debug/java/pt/it/porto/mydiabetes/ui/activities/Charts.java
+++ app/src/debug/java/pt/it/porto/mydiabetes/ui/activities/Charts.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 
 import pt.it.porto.mydiabetes.R;
+import pt.it.porto.mydiabetes.ui.charts.data.*;
+import pt.it.porto.mydiabetes.ui.charts.data.Weight;
 
 public class Charts extends BaseActivity {
 
@@ -16,12 +18,34 @@ public class Charts extends BaseActivity {
 	}
 
 	public void singleData(View view) {
-		Intent intent=new Intent(this, SingleDataChartActivity.class);
+		Intent intent = new Intent(this, SingleDataChartActivity.class);
 		startActivity(intent);
 	}
 
 	public void multiData(View view) {
-		Intent intent=new Intent(this, MultiDataChartActivity.class);
+		Intent intent = new Intent(this, MultiDataChartActivity.class);
+		startActivity(intent);
+	}
+
+
+	public void weight(View view) {
+		Intent intent = new Intent(this, MultiDataChartActivity.class);
+		Bundle bundle=new Bundle();
+		bundle.putParcelable(MultiDataChartActivity.EXTRAS_CHART_DATA, new Weight(this));
+		intent.putExtras(bundle);
+		startActivity(intent);
+	}
+
+	public void carbs(View view) {
+		Intent intent = new Intent(this, MultiDataChartActivity.class);
+		Bundle bundle=new Bundle();
+		bundle.putParcelable(MultiDataChartActivity.EXTRAS_CHART_DATA, new Carbs(this));
+		intent.putExtras(bundle);
+		startActivity(intent);
+	}
+
+	public void test(View view) {
+		Intent intent = new Intent(this, ListTestActivity.class);
 		startActivity(intent);
 	}
 }
