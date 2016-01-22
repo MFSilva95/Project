fb2e2a4cc1637b71b1e7885a474e49f637197d66
Eduardo Soares <eduardosoares.pt@gmail.com>
2016-01-22 15:42:13 +0000

    Added code do save change made in a logbook entry

--- app/src/main/java/pt/it/porto/mydiabetes/ui/activities/DetailLogbookActivity.java
+++ app/src/main/java/pt/it/porto/mydiabetes/ui/activities/DetailLogbookActivity.java
@@ -3,10 +3,12 @@ package pt.it.porto.mydiabetes.ui.activities;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.ImageSpan;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
@@ -19,6 +21,7 @@ import pt.it.porto.mydiabetes.database.DB_Write;
 import pt.it.porto.mydiabetes.ui.listAdapters.CarbsDataBinding;
 import pt.it.porto.mydiabetes.ui.listAdapters.GlycemiaDataBinding;
 import pt.it.porto.mydiabetes.ui.listAdapters.InsulinRegDataBinding;
+import pt.it.porto.mydiabetes.ui.listAdapters.NoteDataBinding;
 import pt.it.porto.mydiabetes.utils.InsulinCalculator;
 
 public class DetailLogbookActivity extends BaseMealActivity {
@@ -174,7 +177,7 @@ public class DetailLogbookActivity extends BaseMealActivity {
 					}).show();
 			return true;
 		} else if (item.getItemId() == R.id.menuItem_LogbookDetail_EditSave) {
-			// TODO support edits in logbook detail
+			saveData();
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
@@ -314,4 +317,247 @@ public class DetailLogbookActivity extends BaseMealActivity {
 	InsulinCalculator getInsulinCalculator() {
 		return insulinCalculator;
 	}
+
+	public void saveData() {
+		DB_Write reg = new DB_Write(this);
+		boolean deleteCarbs = false;
+		boolean deleteBg = false;
+		boolean deleteIns = false;
+		int itemsToDelete = 0;
+
+		int user_id=-1;
+		if(carbsData!=null){
+			user_id=carbsData.getId_User(); // why is this method different from the other two?
+		}else if(glycemiaData!=null){
+			user_id=glycemiaData.getIdUser();
+		} else if(insulinData!=null){
+			user_id=insulinData.getIdUser();
+		} else {
+			// go to database and get it!
+			// it shouldn't be necessary, if we are where some of this values are defined
+			Log.e("DetailLogbookActivity", "carbsData, glycemiaData and insulinData are all null");
+		}
+
+		String date = getDate();
+		String time = getTime();
+		String note = getNote();
+		Uri imgUri = getImgUri();
+
+		// IDs
+		int carbsRegId = 0;
+		int glycemiaRegId = 0;
+		int insulinRegId = 0;
+
+
+		//nota id nao existe e tem de ser criado
+		if (noteId == -1 && !note.isEmpty()) {
+			NoteDataBinding n = new NoteDataBinding();
+			n.setNote(note);
+			noteId = reg.Note_Add(n);
+		} else if (noteId != -1) {        //nota id existe e o texto foi apagado/alterado
+			NoteDataBinding n = new NoteDataBinding();
+			n.setNote(note);
+			n.setId(noteId);
+			reg.Note_Update(n);
+		}
+
+
+		//carbs
+		//carbs id existe e foi apagado
+		String tagSelected = getPhaseOfDay();
+		DB_Read rdb = new DB_Read(this);
+		int tagId = rdb.Tag_GetIdByName(tagSelected);
+		rdb.close();
+
+		if (carbsData != null && insulinCalculator.getCarbs() == 0) {
+			deleteCarbs = true;
+			itemsToDelete++;
+			carbsRegId = carbsData.getId();
+		} else if (carbsData == null && insulinCalculator.getCarbs() != 0) {//carbs id nao existe e tem de ser criad
+			carbsData = new CarbsDataBinding();
+			if (noteId != -1) {
+				carbsData.setId_Note(noteId);
+			}
+
+			carbsData.setId_User(user_id);
+			carbsData.setCarbsValue(insulinCalculator.getCarbs());
+			carbsData.setId_Tag(tagId);
+			carbsData.setPhotoPath(imgUri != null ? imgUri.getPath() : null); // /data/MyDiabetes/yyyy-MM-dd HH.mm.ss.jpg
+			carbsData.setDate(date);
+			carbsData.setTime(time);
+
+			reg.Carbs_Save(carbsData);
+		} else if (carbsData != null && insulinCalculator.getCarbs() != 0) {//carbs id existe e valor está igual ou foi alterado
+			carbsRegId = carbsData.getId();
+			if (noteId != -1) {
+				carbsData.setId_Note(noteId);
+			}
+
+			carbsData.setCarbsValue(insulinCalculator.getCarbs());
+			carbsData.setId_Tag(tagId);
+			carbsData.setPhotoPath(imgUri != null ? imgUri.getPath() : null); // /data/MyDiabetes/yyyy-MM-dd HH.mm.ss.jpg
+			carbsData.setDate(date);
+			carbsData.setTime(time);
+
+			reg.Carbs_Update(carbsData);
+		}
+
+		//glycemia
+		//glycemia  exists and was deleted
+		if (glycemiaData != null && insulinCalculator.getGlycemia() == 0) {
+			glycemiaRegId = glycemiaData.getId();
+			deleteBg = true;
+			itemsToDelete++;
+		} else if (glycemiaData == null && insulinCalculator.getGlycemia() != 0) {//bg id nao existe e tem de ser criado
+			glycemiaData = new GlycemiaDataBinding();
+			if (noteId != -1) {
+				glycemiaData.setIdNote(noteId);
+			}
+			glycemiaData.setIdUser(carbsData.getId_User());
+			glycemiaData.setValue(insulinCalculator.getGlycemia());
+			glycemiaData.setDate(date);
+			glycemiaData.setTime(time);
+
+			glycemiaData.setIdTag(tagId);
+
+			glycemiaRegId = reg.Glycemia_Save(glycemiaData);
+		} else if (glycemiaData != null && insulinCalculator.getGlycemia() != 0) {        //bg id existe e valor está igual ou foi alterado
+			glycemiaRegId = glycemiaData.getId();
+			if (noteId != -1) {
+				glycemiaData.setIdNote(noteId);
+			}
+			glycemiaData.setIdUser(carbsData.getId_User());
+			glycemiaData.setValue(insulinCalculator.getGlycemia());
+			glycemiaData.setDate(date);
+			glycemiaData.setTime(time);
+			glycemiaData.setIdTag(tagId);
+
+			reg.Glycemia_Update(glycemiaData);
+		}
+
+
+		//insulin
+		rdb = new DB_Read(this);
+		int insulinId = rdb.Insulin_GetByName(getInsulin()).getId();
+		rdb.close();
+		float insulinIntake = getInsulinIntake();
+
+		//ins id existe e foi apagado
+		if (insulinData != null && Float.compare(insulinIntake, 0) == 0) {
+			insulinRegId = insulinData.getId();
+			deleteIns = true;
+			itemsToDelete++;
+		} else if (insulinData == null && Float.compare(insulinIntake, 0) != 0) {        //ins id nao existe e tem de ser criado
+			insulinData = new InsulinRegDataBinding();
+			if (noteId != -1) {
+				insulinData.setIdNote(noteId);
+			}
+
+			insulinData.setIdUser(carbsData.getId_User());
+			insulinData.setIdInsulin(insulinId);
+			insulinData.setIdBloodGlucose(glycemiaRegId != -1 ? glycemiaRegId : -1);
+			insulinData.setDate(date);
+			insulinData.setTime(time);
+			insulinData.setTargetGlycemia(insulinCalculator.getInsulinTarget());
+			insulinData.setInsulinUnits(insulinIntake);
+
+			insulinData.setIdTag(tagId);
+
+			insulinRegId = reg.Insulin_Save(insulinData);
+		} else if (insulinData != null && Float.compare(insulinIntake, 0) != 0) {//ins id existe e valor está igual ou foi alterado
+			insulinRegId = insulinData.getId();
+			if (noteId != -1) {
+				insulinData.setIdNote(noteId);
+			}
+
+			insulinData.setIdInsulin(insulinId);
+			insulinData.setIdBloodGlucose(glycemiaRegId != -1 ? glycemiaRegId : -1);
+			insulinData.setDate(date);
+			insulinData.setTime(time);
+			insulinData.setTargetGlycemia(insulinCalculator.getInsulinTarget());
+			insulinData.setInsulinUnits(insulinIntake);
+
+			insulinData.setIdTag(tagId);
+
+			reg.Insulin_Update(insulinData);
+		}
+		reg.close();
+
+		if (deleteCarbs || deleteBg || deleteIns) {
+			final Context c = this;
+			String message;
+			if (itemsToDelete == 1) {
+				message = getString(R.string.deleteConfirmationSingular);
+			} else {
+				message = getString(R.string.deleteConfirmationPlural);
+			}
+
+			if (deleteBg) {
+				message += "\n" + "- " + getString(R.string.deleteGlycemiaReg);
+			}
+			if (deleteCarbs) {
+				message += "\n" + "- " + getString(R.string.deleteCarbsReg);
+			}
+			if (deleteIns) {
+				message += "\n" + "- " + getString(R.string.deleteInsulinReg);
+			}
+
+			final boolean delCh = deleteCarbs;
+			final boolean delBg = deleteBg;
+			final boolean delIns = deleteIns;
+
+			final int finalCarbsRegId = carbsRegId;
+			final int finalInsulinRegId = insulinRegId;
+			final int finalGlycemiaRegId = glycemiaRegId;
+			new AlertDialog.Builder(this)
+					.setTitle(getString(R.string.deleteAlert))
+					.setMessage(message)
+					.setPositiveButton(getString(R.string.positiveButton), new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int whichButton) {
+
+							try {
+								DB_Write wdb = new DB_Write(c);
+								if (delCh && delBg && delIns) {
+									wdb.Logbook_Delete(finalCarbsRegId, finalInsulinRegId, finalGlycemiaRegId, noteId);
+								} else {
+									if (delCh) {
+										wdb.Logbook_DeleteOnSave(finalCarbsRegId, -1, -1, -1);
+									}
+									if (delBg) {
+										if (insulinData != null) {
+											wdb.Logbook_DeleteOnSave(-1, -1, finalGlycemiaRegId, finalInsulinRegId);
+
+										} else {
+											wdb.Logbook_DeleteOnSave(-1, -1, finalGlycemiaRegId, -1);
+										}
+									}
+									if (delIns) {
+										wdb.Logbook_DeleteOnSave(-1, finalInsulinRegId, -1, -1);
+									}
+								}
+								wdb.close();
+								finish();
+							} catch (Exception e) {
+
+								Toast.makeText(c, getString(R.string.deleteException), Toast.LENGTH_LONG).show();
+								Log.d("Excepção", e.getMessage());
+								//Log.d("LocalizedMessage",e.getLocalizedMessage());
+								//Log.d("trace",e.getStackTrace().toString());
+								//e.printStackTrace();
+
+							}
+
+
+						}
+					})
+					.setNegativeButton(getString(R.string.negativeButton), new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int whichButton) {
+							// Do nothing.
+						}
+					}).show();
+		} else {
+			finish();
+		}
+	}
+
 }
