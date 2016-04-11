7a636304aa94c857699a319df098fc4164298107
Eduardo Soares <eduardosoares.pt@gmail.com>
2016-04-11 15:32:27 +0100

    Changed weight list activity to extend base list range activity

--- app/src/main/java/pt/it/porto/mydiabetes/ui/activities/Exercise.java
+++ app/src/main/java/pt/it/porto/mydiabetes/ui/activities/Exercise.java
@@ -1,76 +1,21 @@
 package pt.it.porto.mydiabetes.ui.activities;
 
-import android.app.DialogFragment;
 import android.content.Intent;
 import android.database.Cursor;
-import android.os.Bundle;
-import android.support.v4.app.NavUtils;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.ListView;
+import android.widget.ListAdapter;
 
 import java.util.Calendar;
 
 import pt.it.porto.mydiabetes.R;
 import pt.it.porto.mydiabetes.database.ListsDataDb;
 import pt.it.porto.mydiabetes.database.MyDiabetesStorage;
-import pt.it.porto.mydiabetes.ui.dialogs.DatePickerFragment;
 import pt.it.porto.mydiabetes.ui.listAdapters.ExerciseRegAdapter;
 import pt.it.porto.mydiabetes.utils.DateUtils;
 
 
-public class Exercise extends BaseActivity {
-
-	ListView exercisesList;
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_exercise);
-		// Show the Up button in the action bar.
-		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-		FillDates();
-
-		exercisesList = (ListView) findViewById(R.id.ExercisesActivityList);
-		fillListView(exercisesList);
-
-
-		EditText datefrom = (EditText) findViewById(R.id.et_DataFrom);
-		EditText dateto = (EditText) findViewById(R.id.et_DataTo);
-		datefrom.addTextChangedListener(new TextWatcher() {
-			@Override
-			public void onTextChanged(CharSequence s, int start, int before, int count) {
-				fillListView(exercisesList);
-			}
-
-			@Override
-			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-			}
-
-			@Override
-			public void afterTextChanged(Editable s) {
-			}
-		});
-		dateto.addTextChangedListener(new TextWatcher() {
-			@Override
-			public void onTextChanged(CharSequence s, int start, int before, int count) {
-				fillListView(exercisesList);
-			}
-
-			@Override
-			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-			}
-
-			@Override
-			public void afterTextChanged(Editable s) {
-			}
-		});
-	}
-
+public class Exercise extends BaseListRangeActivity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -82,9 +27,6 @@ public class Exercise extends BaseActivity {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
-			case android.R.id.home:
-				NavUtils.navigateUpFromSameTask(this);
-				return true;
 			case R.id.menuItem_Exercise_Add:
 				Intent intent = new Intent(this, ExerciseDetail.class);
 				startActivity(intent);
@@ -94,38 +36,23 @@ public class Exercise extends BaseActivity {
 		return super.onOptionsItemSelected(item);
 	}
 
-	public void FillDates() {
-		EditText dateago = (EditText) findViewById(R.id.et_DataFrom);
+	@Override
+	String getBaseStartDate() {
 		Calendar calendar = Calendar.getInstance();
 		calendar.add(Calendar.DAY_OF_YEAR, -8);
-
-		dateago.setText(DateUtils.getFormattedDate(calendar));
-
-		EditText datenow = (EditText) findViewById(R.id.et_DataTo);
-		calendar = Calendar.getInstance();
-		datenow.setText(DateUtils.getFormattedDate(calendar));
+		return DateUtils.getFormattedDate(calendar);
 	}
 
-	public void showDatePickerDialogFrom(View v) {
-		DialogFragment newFragment = DatePickerFragment.getDatePickerFragment(R.id.et_DataFrom,
-				DateUtils.getDateCalendar(((EditText) v).getText().toString()));
-		newFragment.show(getFragmentManager(), "DatePicker");
+	@Override
+	String getBaseEndDate() {
+		Calendar calendar = Calendar.getInstance();
+		return DateUtils.getFormattedDate(calendar);
 	}
 
-	public void showDatePickerDialogTo(View v) {
-		DialogFragment newFragment = DatePickerFragment.getDatePickerFragment(R.id.et_DataTo,
-				DateUtils.getDateCalendar(((EditText) v).getText().toString()));
-		newFragment.show(getFragmentManager(), "DatePicker");
-	}
-
-	public void fillListView(ListView lv) {
-		EditText dateFrom = (EditText) findViewById(R.id.et_DataFrom);
-		EditText dateTo = (EditText) findViewById(R.id.et_DataTo);
-
+	@Override
+	ListAdapter getListAdapter() {
 		ListsDataDb db = new ListsDataDb(MyDiabetesStorage.getInstance(this));
-		Cursor cursor = db.getExerciseRegList(dateFrom.getText().toString(), dateTo.getText().toString());
-
-		lv.setAdapter(new ExerciseRegAdapter(cursor, this));
-		lv.setEmptyView(findViewById(R.id.list_empty));
+		Cursor cursor = db.getExerciseRegList(getStartDate(), getEndDate());
+		return new ExerciseRegAdapter(cursor, this);
 	}
 }
